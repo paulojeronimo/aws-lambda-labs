79727c929d218c8f985c71de5c58bff923a927b0
Paulo Jerônimo <paulojeronimo@gmail.com>
2021-03-21 16:40:47 -0300

    Improved download script

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-repo=aws-lambda-labs
-curl -L https://github.com/paulojeronimo/$repo/archive/refs/heads/main.tar.gz |
-	tar -xz
-mv $repo-main $repo
+set -eou pipefail
+declare -r repo=https://github.com/paulojeronimo/aws-lambda-labs
+declare -r dir=${repo##*/}
+curl -sL $repo/archive/refs/heads/main.tar.gz | tar -xz
+mv $dir-main $dir && cd $_
