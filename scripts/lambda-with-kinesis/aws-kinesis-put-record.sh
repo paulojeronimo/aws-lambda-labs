f7ffa8f723a5be3756fdb7c95a33ebf47472d08a
Paulo Jerônimo <paulojeronimo@gmail.com>
2021-03-23 07:41:55 -0300

    Fixed scripts/lambda-with-kinesis/aws-kinesis-put-record.sh

--- scripts/lambda-with-kinesis/aws-kinesis-put-record.sh
+++ scripts/lambda-with-kinesis/aws-kinesis-put-record.sh
@@ -1,4 +1,5 @@
 aws kinesis put-record \
 	--stream-name lambda-stream \
 	--partition-key 1 \
+	--cli-binary-format raw-in-base64-out \
 	--data "Hello, this is a test from aws-lambda-labs at `date +%T`."
